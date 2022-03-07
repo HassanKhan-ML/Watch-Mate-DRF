6ae10417d7e72332794927ea037a8e914e526dc3
hassan jamal khan <hassan.jamal@mobylogix.com>
2022-03-07 23:04:08 +0500

    working on drf crud

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from watchlist_app.models import Movie
 
 # from watchlist_app.models import WatchList,SteamPlatform
 
@@ -8,13 +9,12 @@ class MovieSerializers(serializers.Serializer):
   description = serializers.CharField()
   active = serializers.BooleanField()
 
-#   class Meta:
-#     model = SteamPlatform
-#     fields = "__all__"
+  def create(self, validated_data):
+    return Movie.objects.create(**validated_data)
 
-# class WatchListSerializers(serializers.ModelSerializer):
-#   # len_name = serializers.SerializerMethodField()
-  
-#   class Meta:
-#     model =  WatchList
-#     fields = "__all__"
+  def update(self,instance,validated_data):
+    instance.name = validated_data.get('name',instance.name)
+    instance.description = validated_data.get('description',instance.description)
+    instance.active = validated_data.get('active',instance.active) 
+    instance.save() 
+    return instance
