ae474d8b6546dd7d912dfc2aa74a88fc1091fc2a
hassan jamal khan <hassan.jamal@mobylogix.com>
2022-03-15 13:22:48 +0500

    working on status code drf

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializers
+from rest_framework import status
+
 
 @api_view(['GET','POST'])          # Empty default request is get
 def movie_list(request):
@@ -23,23 +25,27 @@ def movie_list(request):
 @api_view(['GET','PUT','DELETE'])
 def movie_details(request,pk):
   if request.method == 'GET':
-    movie = Movie.objects.get(pk=pk)
+    try:
+      movie = Movie.objects.get(pk=pk)
+    except:
+      return Response({'Error': 'Movie Not Found '}, status=status.HTTP_400_BAD_REQUEST)
+    
     serializer = MovieSerializers(movie)
     return Response(serializer.data)
   
   if request.method == "PUT":
     movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializers(movie,data=request.data)
+    serializer = MovieSerializers(movie, data=request.data)
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data)
     else:
-      return Response(serializer.errors)
+      return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
   if request.method == "DELETE":
     movie = Movie.objects.get(pk=pk)
     movie.delete()
-    return Response()
+    return Response(status= status.HTTP_204_NO_CONTENT)
 
 
 
