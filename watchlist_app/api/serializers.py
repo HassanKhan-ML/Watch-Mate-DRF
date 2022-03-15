efff260d1b67bf9c080d15b5e8987323873b8c71
hassan jamal khan <hassan.jamal@mobylogix.com>
2022-03-15 18:43:27 +0500

    working on nested serializers

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -3,19 +3,20 @@ from watchlist_app.models import WatchList, StreamPlatform
 
 # from watchlist_app.models import WatchList,SteamPlatform
 
+class WatchListSerializer(serializers.ModelSerializer):
+  
+  class Meta:
+    model = WatchList
+    fields = '__all__'
+    
 class StreamPlatformSerializer(serializers.ModelSerializer):
+  
+  watchlist = WatchListSerializer(many = True, read_only= True)
 
   class Meta:
     model = StreamPlatform
     fields = '__all__'
 
-class WatchListSerializer(serializers.ModelSerializer):
-  
-  # len_name = serializers.SerializerMethodField()
-    
-  class Meta:
-    model = WatchList
-    fields = '__all__'
     
 # def name_length(value):
 #   if len(value) < 2:
